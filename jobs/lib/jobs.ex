828789442c34a46da4d54377c53e0d819a246a3f
dhivyabharathii <DhivyaBharathi.S@bigthinkcode.com>
2024-02-21 16:07:32 +0530

    Parsing the json correctly, storing the latency in a file

--- jobs/lib/jobs.ex
+++ jobs/lib/jobs.ex
@@ -26,36 +26,10 @@
         update["farm_id"] == 3
       end)
     end
-
     @doc """
-    Calculates the time difference between update and job timestamps.
+    Extracting the necessary details drom the given data.
+    When tab is found , moving the contents to the next line
     """
-    def calculate_time_difference(updates) do
-      Enum.reduce(updates, %{}, fn update, acc ->
-        timestamp = parse_iso8601(update["timestamp"])
-        Enum.reduce(update["jobs"], acc, fn job, acc ->
-          job_id = job["Id"]
-          job_timestamp = parse_iso8601(job["UpdatedAt"])
-          time_difference_in_seconds = abs(NaiveDateTime.diff(job_timestamp, timestamp))
-
-          case Map.get(acc, job_id) do
-            nil -> Map.put(acc, job_id, time_difference_in_seconds)
-            existing_time_difference when existing_time_difference > time_difference_in_seconds ->
-              Map.put(acc, job_id, time_difference_in_seconds)
-            _ -> acc
-          end
-        end)
-      end)
-    end
-
-    defp parse_iso8601(timestamp) do
-      case NaiveDateTime.from_iso8601(timestamp) do
-        {:ok, dt} -> dt
-        :error -> nil
-      end
-    end
-
-
     def format_file(input_file, output_file) do
       input_stream = File.stream!(input_file)
       output_stream = File.open!(output_file, [:write])
@@ -75,7 +49,11 @@
 
       File.close(output_stream)
     end
-    def write_lines_multiples_of_three(input_file, output_file) do
+
+    @doc """
+    Write the necessary line in a file
+    """
+    def write_lines(input_file, output_file) do
       input_stream = File.stream!(input_file)
       output_stream = File.open!(output_file, [:write])
 
@@ -89,13 +67,58 @@
 
       File.close(output_stream)
     end
+
+    @doc """
+    Calculates the time difference between update and job timestamps.
+    """
+    def calculate_time_difference(updates) do
+      final_map = Enum.reduce(updates, %{}, fn update, acc ->
+        timestamp = parse_iso8601(update["timestamp"])
+        Enum.reduce(update["jobs"], acc, fn job, acc ->
+          job_id = job["Id"]
+          job_timestamp = parse_iso8601(job["UpdatedAt"])
+          time_difference_in_seconds = abs(NaiveDateTime.diff(job_timestamp, timestamp))
+
+          case Map.get(acc, job_id) do
+            nil -> Map.put(acc, job_id, time_difference_in_seconds)
+            existing_time_difference when existing_time_difference > time_difference_in_seconds ->
+              Map.put(acc, job_id, time_difference_in_seconds)
+            _ -> acc
+          end
+        end)
+      end)
+
+      write_to_file(final_map, "Latency.txt")
+      final_map
+    end
+
+    defp write_to_file(map, file_path) do
+      {:ok, file} = File.open(file_path, [:write])
+
+      Enum.each(map, fn {job_id, time_diff} ->
+        formatted_line = "#{job_id} => #{time_diff}\n"
+        IO.write(file, formatted_line)
+      end)
+
+      File.close(file)
+    end
+
+    defp parse_iso8601(timestamp) do
+      case NaiveDateTime.from_iso8601(timestamp) do
+        {:ok, dt} -> dt
+        :error -> nil
+      end
+    end
   end
-  file_path = "sample.json"
+
+
+
+  # Jobs.format_file("v1_jobs_1h.txt","output.txt")
+  # Jobs.write_lines("output.txt","final1.json")
+  file_path = "final1.json"
   {:ok, file_content} = Jobs.read(file_path)
   updates = Jason.decode!(file_content)
   filtered_updates = Jobs.filter_updates(updates)
-  IO.inspect(filtered_updates)
   time_difference=Jobs.calculate_time_difference(filtered_updates)
   IO.inspect(time_difference)
-  # Jobs.format_file("v1_jobs_1h.txt","output.txt")
-  # Jobs.write_lines_multiples_of_three("output.txt","final.txt")
+  IO.inspect(filtered_updates)
